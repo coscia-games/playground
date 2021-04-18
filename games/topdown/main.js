8258a8581cd96616a026b45dc517e7995f963927
Adam Coscia <47363758+AdamCoscia@users.noreply.github.com>
2021-04-18 17:31:38 -0400

    fix inputController

--- games/topdown/main.js
+++ games/topdown/main.js
@@ -1,3 +1,5 @@
+import { InputController } from "../../modules/inputController.js";
+
 /** GLOBALS */
 
 // Document Elements
@@ -5,6 +7,7 @@ var canvas = document.getElementById("board");
 var context = canvas.getContext("2d");
 
 // Game Objects
+var inputController = new InputController();
 var game; // Game properties
 var usr; // usr properties
 var pc; // pc properties
@@ -36,171 +39,19 @@ function clearCanvas() {
 
 function setDefaults() {
   game = {
-    fps: 30,
-    gravity: 0.6,
-    platforms: [],
-    ground: canvas.height / 2 + 100,
+    fps: 60,
   };
-  usr = {
-    holdLeft: false,
-    holdRight: false,
-  };
-  pc = {
-    x: 0,
-    y: 0,
-    h: 25,
-    w: 10,
-    vx: 0,
-    vy: 0,
-    onGround: true,
-  };
+  usr = {};
+  pc = {};
 }
 
 function startGame() {
-  // add button controls
-  document.addEventListener("keydown", keyDown);
-  document.addEventListener("keyup", keyUp);
-  document.getElementById("leftarrow").addEventListener("mousedown", onPressLeft);
-  document.getElementById("leftarrow").addEventListener("mouseup", onDepressLeft);
-  document.getElementById("leftarrow").addEventListener("touchstart", onPressLeft);
-  document.getElementById("leftarrow").addEventListener("touchend", onDepressLeft);
-  document.getElementById("uparrow").addEventListener("mousedown", onPressUp);
-  document.getElementById("uparrow").addEventListener("mouseup", onDepressUp);
-  document.getElementById("uparrow").addEventListener("touchstart", onPressUp);
-  document.getElementById("uparrow").addEventListener("touchend", onDepressUp);
-  document.getElementById("rightarrow").addEventListener("mousedown", onPressRight);
-  document.getElementById("rightarrow").addEventListener("mouseup", onDepressRight);
-  document.getElementById("rightarrow").addEventListener("touchstart", onPressRight);
-  document.getElementById("rightarrow").addEventListener("touchend", onDepressRight);
-  // create platforms
-  for (i = 0; i < 20; i++) {
-    game.platforms.push({
-      x: i * 75,
-      y: Math.floor(Math.random() * 30) + 20,
-      w: Math.floor(Math.random() * (40 - 20 + 1)) + 30,
-      h: 10,
-    });
-  }
+  // attach inputs to event listenters
+  inputController.attachListeners();
   // run game
   setInterval(gameLoop, 1000 / game.fps);
 }
 
-function onPressLeft() {
-  usr.holdLeft = true;
-}
-
-function onDepressLeft() {
-  usr.holdLeft = false;
-}
-
-function onPressUp() {
-  if (pc.onGround) pc.vy = 6;
-}
-
-function onDepressUp() {
-  return;
-}
-
-function onPressRight() {
-  usr.holdRight = true;
-}
-
-function onDepressRight() {
-  usr.holdRight = false;
-}
-
-function keyDown(evt) {
-  evt.preventDefault();
-  switch (evt.keyCode) {
-    case 37: // Left key
-      onPressLeft();
-      break;
-    case 38: // Up key
-      onPressUp();
-      break;
-    case 39: // Right key
-      onPressRight();
-      break;
-  }
-}
-
-function keyUp(evt) {
-  evt.preventDefault();
-  switch (evt.keyCode) {
-    case 37: // Left key
-      onDepressLeft();
-      break;
-    case 38: // Up key
-      onDepressUp();
-      break;
-    case 39: // Right key
-      onDepressRight();
-      break;
-  }
-}
-
 function gameLoop() {
-  // Update pc x velocity.
-  if (usr.holdLeft && usr.holdRight) {
-    pc.vy = 0;
-  } else if (usr.holdLeft) {
-    pc.vx = -3;
-  } else if (usr.holdRight) {
-    pc.vx = 3;
-  } else if (pc.onGround) {
-    pc.vx *= 0.8; // slow pc down while on ground
-  }
-
-  // Update pc y velocity.
-  if (pc.vy > -6) {
-    pc.vy -= game.gravity;
-  } else {
-    pc.vy = -6; // cap the descent velocity
-  }
-
-  // Update pc position.
-  pc.x += pc.vx;
-  pc.y += pc.vy;
-
-  // Handle object collision.
-  pc.onGround = false;
-  if (pc.y <= 0) {
-    // charcter below ground
-    pc.y = 0;
-    pc.onGround = true;
-  } else {
-    for (i = 0; i < game.platforms.length; i++) {
-      if (
-        pc.x > game.platforms[i].x &&
-        pc.x < game.platforms[i].x + game.platforms[i].w &&
-        pc.y < game.platforms[i].y &&
-        pc.y > game.platforms[i].y - game.platforms[i].h
-      ) {
-        // pc inside platform => put them on platform
-        pc.y = game.platforms[i].y;
-        pc.vy = 0;
-        pc.onGround = true;
-        break;
-      }
-    }
-  }
-
-  // Clear Canvas
-  clearCanvas();
-  // Draw pc
-  context.fillStyle = "red";
-  context.fillRect((canvas.width - pc.w) / 2, game.ground - pc.h, pc.w, pc.h);
-  // Draw Ground
-  context.fillStyle = "black";
-  context.fillRect(0, game.ground + pc.y, canvas.width, canvas.height - (game.ground + pc.y));
-  // Draw Elements
-  context.fillStyle = "green";
-  for (i = 0; i < game.platforms.length; i++) {
-    context.fillRect(
-      canvas.width / 2 + game.platforms[i].x - pc.x,
-      game.ground + pc.y - game.platforms[i].y,
-      game.platforms[i].w,
-      game.platforms[i].h
-    );
-  }
+  // TODO
 }
---
+++ modules/inputController.js
@@ -0,0 +1,121 @@
+export class InputController {
+  eventKeyMap;
+  eventCodeMap;
+  clickMap;
+
+  constructor() {
+    let context = this;
+
+    /**
+     * Initialize event key map. Keys are dependent on the localization, so
+     * the key should represent the same character being pressed on the
+     * keyboard, even if the locations of the keys aren't the same.
+     */
+    this.eventKeyMap = {
+      w: {},
+      a: {},
+      s: {},
+      d: {},
+      p: {},
+      " ": {},
+      ArrowLeft: {},
+      ArrowUp: {},
+      ArrowRight: {},
+      ArrowDown: {},
+    };
+    Object.getOwnPropertyNames(this.eventKeyMap).forEach((key) => {
+      let value = context.eventKeyMap[key];
+      value.key = key; // set reference to key name
+    });
+
+    /**
+     * Initialize event code map. Codes are not dependent on the localization,
+     * so the code should be in the same location on the keyboard even if the
+     * key binding itself is not the same.
+     */
+    this.eventCodeMap = {
+      KeyW: {},
+      KeyA: {},
+      KeyS: {},
+      KeyD: {},
+      KeyP: {},
+      Space: {},
+      ArrowLeft: {},
+      ArrowUp: {},
+      ArrowRight: {},
+      ArrowDown: {},
+    };
+    Object.getOwnPropertyNames(this.eventCodeMap).forEach((key) => {
+      let value = context.eventCodeMap[key];
+      value.code = key; // set reference to key name
+      value.up = () => console.log(`keyup > ${key}`);
+      value.down = () => console.log(`keydown > ${key}`);
+    });
+
+    /**
+     * Initialize on-screen click inputs map. These names are ids for elements
+     * in the HTML that should recieve event listeners.
+     */
+    this.clickMap = {
+      leftarrow: {},
+      uparrow: {},
+      rightarrow: {},
+      downarrow: {},
+    };
+    Object.getOwnPropertyNames(this.clickMap).forEach((key) => {
+      let value = context.clickMap[key];
+      value.id = key; // set reference to key name
+      value.ref = document.getElementById(key);
+      value.press = () => console.log(`mousedown touchstart > ${key}`);
+      value.depress = () => console.log(`mouseup touchend > ${key}`);
+    });
+  }
+
+  attachListeners() {
+    let context = this;
+    document.addEventListener(
+      "keydown",
+      (e) => {
+        e.preventDefault();
+        if (!e.repeat) {
+          if (context.eventKeyMap[e.key] && context.eventKeyMap[e.key].hasOwnProperty("down")) {
+            context.eventKeyMap[e.key].down();
+          }
+          if (context.eventCodeMap[e.code] && context.eventCodeMap[e.code].hasOwnProperty("down")) {
+            context.eventCodeMap[e.code].down();
+          }
+        }
+      },
+      false
+    );
+    document.addEventListener(
+      "keyup",
+      (e) => {
+        e.preventDefault();
+        if (!e.repeat) {
+          if (context.eventKeyMap[e.key] && context.eventKeyMap[e.key].hasOwnProperty("up")) {
+            context.eventKeyMap[e.key].up();
+          }
+          if (context.eventCodeMap[e.code] && context.eventCodeMap[e.code].hasOwnProperty("up")) {
+            context.eventCodeMap[e.code].up();
+          }
+        }
+      },
+      false
+    );
+    for (const [_, el] of Object.entries(this.clickMap)) {
+      el.ref.addEventListener("mousedown", el.press, {
+        passive: false,
+      });
+      el.ref.addEventListener("touchstart", el.press, {
+        passive: false,
+      });
+      el.ref.addEventListener("mouseup", el.depress, {
+        passive: false,
+      });
+      el.ref.addEventListener("touchend", el.depress, {
+        passive: false,
+      });
+    }
+  }
+}
