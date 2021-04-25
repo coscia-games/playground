052b982e9b167342459a5d40fb1ef828b3ff5279
Adam Coscia <47363758+AdamCoscia@users.noreply.github.com>
2021-04-25 18:18:06 -0400

    basic pixi render

--- games/topdown/main.js
+++ games/topdown/main.js
@@ -13,8 +13,8 @@ import { InputController } from "../../modules/inputController.js";
   // create new pixi app
   var app = new PIXI.Application({
     antialias: true, // default: false
-    transparent: false, // default: false
-    resolution: 1, // default: 1
+    backgroundColor: 0x1099bb,
+    resolution: window.devicePixelRatio || 1,
   });
 
   // create app container and methods
@@ -38,18 +38,47 @@ import { InputController } from "../../modules/inputController.js";
   };
 
   Application.init = function () {
-    // app.renderer.autoResize = true;
+    // set app sizes and add
     appContainer.onResize();
-    // Add the canvas that Pixi automatically created for you to the HTML document
     appContainer.$elem.append(app.view);
-    // attach event listeners and handlers to fullscreen button
+
+    // attach fullscreen event listeners and
     app.renderer.view.onfullscreenchange = (event) =>
       appContainer.onResize(document.fullscreenElement === event.target);
     $("#fs-btn").on("click", () =>
       !document.fullscreenElement ? app.renderer.view.requestFullscreen() : document.exitFullscreen()
     );
-    // resize app on window resize
     $(window).on("resize", () => (!document.fullscreenElement ? appContainer.onResize() : {}));
+
+    // load sprites to the stage
+    const container = new PIXI.Container();
+
+    app.stage.addChild(container);
+
+    // Create a new texture
+    const texture = PIXI.Texture.from("assets/man.png");
+
+    // Create a 5x5 grid of bunnies
+    for (let i = 0; i < 25; i++) {
+      const man = new PIXI.Sprite(texture);
+      man.anchor.set(0.5);
+      man.x = (i % 5) * 40;
+      man.y = Math.floor(i / 5) * 40;
+      container.addChild(man);
+    }
+
+    // Move container to the center
+    container.x = app.screen.width / 2;
+    container.y = app.screen.height / 2;
+
+    // Center man sprite in local container coordinates
+    container.pivot.x = container.width / 2;
+    container.pivot.y = container.height / 2;
+
+    // Listen for animate update
+    app.ticker.add((delta) => {
+      container.rotation -= 0.01 * delta;
+    });
   };
 })((window.Application = window.Application || {}));
 
