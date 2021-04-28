0e3b123d448642b00e53313dbc9cf652640ca6c4
Adam Coscia <47363758+AdamCoscia@users.noreply.github.com>
2021-04-27 22:55:00 -0400

    basic movement!

--- games/topdown/main.js
+++ games/topdown/main.js
@@ -40,8 +40,7 @@ var assetsMap = {
     },
     onResize: function (isFullscreen = false) {
       if (isFullscreen) {
-        // use entire window
-        pixiApp.renderer.resize(window.innerWidth, window.innerHeight);
+        pixiApp.renderer.resize(window.innerWidth, window.innerHeight); // use entire window
       } else {
         // get nearest resolution to app container's width
         const resolution = resolutionMap.getNearest(this.width);
@@ -51,36 +50,124 @@ var assetsMap = {
     },
   };
 
+  // create player character object to track state
+  var pc = {
+    /** Hidden PIXI Members */
+    _sprite: new PIXI.Sprite(), // PIXI.Sprite
+    _postext: new PIXI.Text(), // PIXI.Text
+    addToStage: function () {
+      pixiApp.stage.addChild(pc._sprite); // add pc to the stage
+      pixiApp.stage.addChild(pc._postext); // add position text to the stage
+    },
+    /** State */
+    direction: "up",
+    /** Visibility */
+    hide: function () {
+      this._sprite.visible = false;
+    },
+    show: function () {
+      this._sprite.visible = true;
+    },
+    get visible() {
+      return this._sprite.visible;
+    },
+    /** Position */
+    get x() {
+      return this._sprite.x;
+    },
+    get y() {
+      return this._sprite.y;
+    },
+    setPos: function (x, y) {
+      this._sprite.position.set(x, y);
+      this._postext.text = `x: ${this._sprite.x}, y: ${this._sprite.y}, facing: ${this.direction}`;
+    },
+    /** Velocity */
+    get vx() {
+      return this._sprite.vx;
+    },
+    get vy() {
+      return this._sprite.vy;
+    },
+    setVel: function (vx, vy) {
+      this._sprite.vx = vx;
+      this._sprite.vy = vy;
+      if (vx < 0 && vy < 0) this.direction = "up-left";
+      if (vx == 0 && vy < 0) this.direction = "up";
+      if (vx > 0 && vy < 0) this.direction = "up-right";
+      if (vx > 0 && vy == 0) this.direction = "right";
+      if (vx > 0 && vy > 0) this.direction = "down-right";
+      if (vx == 0 && vy > 0) this.direction = "down";
+      if (vx < 0 && vy > 0) this.direction = "down-left";
+      if (vx < 0 && vy == 0) this.direction = "left";
+    },
+  };
+
+  // create new input controller
+  var gamepad = new InputController();
+
   /** PRIVATE METHODS */
 
+  function setMoves() {
+    /** Up */
+    gamepad.eventCodeMap.KeyW.down = () => pc.setVel(pc.vx, -1);
+    gamepad.eventCodeMap.ArrowUp.down = () => pc.setVel(pc.vx, -1);
+    gamepad.eventCodeMap.KeyW.up = () => pc.setVel(pc.vx, 0);
+    gamepad.eventCodeMap.ArrowUp.up = () => pc.setVel(pc.vx, 0);
+    /** Right */
+    gamepad.eventCodeMap.KeyD.down = () => pc.setVel(1, pc.vy);
+    gamepad.eventCodeMap.ArrowRight.down = () => pc.setVel(1, pc.vy);
+    gamepad.eventCodeMap.KeyD.up = () => pc.setVel(0, pc.vy);
+    gamepad.eventCodeMap.ArrowRight.up = () => pc.setVel(0, pc.vy);
+    /** Down */
+    gamepad.eventCodeMap.KeyS.down = () => pc.setVel(pc.vx, 1);
+    gamepad.eventCodeMap.ArrowDown.down = () => pc.setVel(pc.vx, 1);
+    gamepad.eventCodeMap.KeyS.up = () => pc.setVel(pc.vx, 0);
+    gamepad.eventCodeMap.ArrowDown.up = () => pc.setVel(pc.vx, 0);
+    /** Left */
+    gamepad.eventCodeMap.KeyA.down = () => pc.setVel(-1, pc.vy);
+    gamepad.eventCodeMap.ArrowLeft.down = () => pc.setVel(-1, pc.vy);
+    gamepad.eventCodeMap.KeyA.up = () => pc.setVel(0, pc.vy);
+    gamepad.eventCodeMap.ArrowLeft.up = () => pc.setVel(0, pc.vy);
+  }
+
   function setScene() {
-    // establish main character
-    let manSprite = assetsMap.sprites["man"];
-    manSprite.anchor.set(0.5, 1); // in center-bottom of body
-    pixiApp.stage.addChild(manSprite);
+    pc._sprite = assetsMap.sprites["man"]; // set pc sprite to man
+    pc._sprite.anchor.set(0.5, 1); // in center-bottom of body
+    pc.setPos(pixiApp.renderer.view.width / 2, pixiApp.renderer.view.height / 2); // put pc in the center of the screen
+    pc.setVel(0, 0); // start pc not moving
+    pc.addToStage(); // add pc to the stage
   }
 
   function drawScene(delta) {
-    let manSprite = assetsMap.sprites["man"];
-    manSprite.position.set(pixiApp.screen.width / 2, pixiApp.screen.height / 2);
+    pc.setPos(pc.x + pc.vx, pc.y + pc.vy); // update pc position using velocity
   }
 
   /** PUBLIC MEMBERS AND METHODS */
 
   Application.init = function () {
     // attach fullscreen event listeners and handlers
-    pixiApp.renderer.view.onfullscreenchange = (event) =>
+    pixiApp.renderer.view.onfullscreenchange = (event) => {
       appContainer.onResize(document.fullscreenElement === event.target);
-    $("#fs-btn").on("click", () =>
-      !document.fullscreenElement ? pixiApp.renderer.view.requestFullscreen() : document.exitFullscreen()
-    );
-    $(window).on("resize", () => (!document.fullscreenElement ? appContainer.onResize() : {}));
+    };
+    $("#fs-btn").on("click", () => {
+      !document.fullscreenElement ? pixiApp.renderer.view.requestFullscreen() : document.exitFullscreen();
+    });
+    $(window).on("resize", () => {
+      !document.fullscreenElement ? appContainer.onResize() : {};
+    });
+
+    // set gamepad controls
+    setMoves();
+    gamepad.attachKeyboardListenersAndHandlers();
+    gamepad.attachKeyboardListenersAndHandlers();
 
     // load resources
     const pixiLoader = PIXI.Loader.shared;
     pixiLoader.add(assetsMap.resources);
     pixiLoader.load((loader, resources) => {
       for (const [name, _] of Object.entries(resources)) {
+        // load sprites from resource list
         assetsMap.sprites[name] = new PIXI.Sprite(resources[name].texture);
       }
       setScene(); // use loaded sprites to set the stage
--- modules/inputController.js
+++ modules/inputController.js
@@ -1,84 +1,103 @@
+/**
+ * List of KeyboardEvent.code values. Codes are not dependent on the
+ * localization, so the code should be in the same location on the keyboard
+ * even if the key binding itself is not the same.
+ */
+const KeyboardEventCodes = [
+  "KeyW",
+  "KeyA",
+  "KeyS",
+  "KeyD",
+  "KeyP",
+  "Space",
+  "Enter",
+  "ArrowLeft",
+  "ArrowUp",
+  "ArrowRight",
+  "ArrowDown",
+];
+
+/**
+ * List of KeyboardEvent.key values. Keys are dependent on the localization,
+ * so the key should represent the same character being pressed on the
+ * keyboard, even if the locations of the keys aren't the same.
+ */
+const KeyboardEventKeys = ["w", "a", "s", "d", "p", " ", "Enter", "ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"];
+
+/**
+ * List of clickable HTML Elements defined on the page. These names are ids
+ * for elements in the HTML that should recieve event listeners.
+ */
+const ClickableElementIds = ["leftarrow", "uparrow", "rightarrow", "downarrow"];
+
+/**
+ * Controller class for keyboard and click inputs.
+ */
 export class InputController {
   eventKeyMap;
+  eventKeyStack;
+
   eventCodeMap;
+  eventCodeStack;
+
   clickMap;
+  clickStack;
 
   constructor() {
-    let context = this;
-
     /**
-     * Initialize event key map. Keys are dependent on the localization, so
-     * the key should represent the same character being pressed on the
-     * keyboard, even if the locations of the keys aren't the same.
+     * Initialize event key map.
      */
-    this.eventKeyMap = {
-      w: {},
-      a: {},
-      s: {},
-      d: {},
-      p: {},
-      " ": {},
-      ArrowLeft: {},
-      ArrowUp: {},
-      ArrowRight: {},
-      ArrowDown: {},
-    };
+    this.eventKeyMap = Object.fromEntries(KeyboardEventKeys.map((x) => [x, {}]));
+    /**
+     * Initialize event key stack. This will track the most recent event key
+     * pressed to prevent overwriting prior key press events.
+     */
+    this.eventKeyStack = [];
+    /**
+     * Initialize event code map.
+     */
+    this.eventCodeMap = Object.fromEntries(KeyboardEventCodes.map((x) => [x, {}]));
+    /**
+     * Initialize event code stack. This will track the most recent event code
+     * pressed to prevent overwriting prior code press events.
+     */
+    this.eventCodeStack = [];
+    /**
+     * Initialize on-screen click inputs map.
+     */
+    this.clickMap = Object.fromEntries(ClickableElementIds.map((x) => [x, {}]));
+    /**
+     * Initialize click stack. This will track the most recent clicks to
+     * prevent overwriting prior click events.
+     */
+    this.clickStack = [];
+  }
+
+  /**
+   * Attaches keydown and keyup event handlers to keystroke event listeners.
+   */
+  attachKeyboardListenersAndHandlers() {
+    let context = this;
     Object.getOwnPropertyNames(this.eventKeyMap).forEach((key) => {
       let value = context.eventKeyMap[key];
       value.key = key; // set reference to key name
     });
-
-    /**
-     * Initialize event code map. Codes are not dependent on the localization,
-     * so the code should be in the same location on the keyboard even if the
-     * key binding itself is not the same.
-     */
-    this.eventCodeMap = {
-      KeyW: {},
-      KeyA: {},
-      KeyS: {},
-      KeyD: {},
-      KeyP: {},
-      Space: {},
-      ArrowLeft: {},
-      ArrowUp: {},
-      ArrowRight: {},
-      ArrowDown: {},
-    };
     Object.getOwnPropertyNames(this.eventCodeMap).forEach((key) => {
       let value = context.eventCodeMap[key];
       value.code = key; // set reference to key name
     });
-
-    /**
-     * Initialize on-screen click inputs map. These names are ids for elements
-     * in the HTML that should recieve event listeners.
-     */
-    this.clickMap = {
-      leftarrow: {},
-      uparrow: {},
-      rightarrow: {},
-      downarrow: {},
-    };
-    Object.getOwnPropertyNames(this.clickMap).forEach((key) => {
-      let value = context.clickMap[key];
-      value.id = key; // set reference to key name
-      value.ref = document.getElementById(key);
-    });
-  }
-
-  attachListeners() {
-    let context = this;
     document.addEventListener(
       "keydown",
       (e) => {
-        e.preventDefault();
+        e.preventDefault(); // prevent default behavior to avoid unexpected page updates
         if (!e.repeat) {
           if (context.eventKeyMap[e.key] && context.eventKeyMap[e.key].hasOwnProperty("down")) {
-            context.eventKeyMap[e.key].down();
+            context.eventKeyMap[e.key].down(); // call down() for key event
+            context.eventKeyStack.push(context.eventKeyMap[e.key]); // add key to key stack
           }
           if (context.eventCodeMap[e.code] && context.eventCodeMap[e.code].hasOwnProperty("down")) {
-            context.eventCodeMap[e.code].down();
+            context.eventCodeMap[e.code].down(); // call down() for code event
+            context.eventCodeStack.push(context.eventCodeMap[e.code]); // add code to code stack
           }
         }
       },
@@ -87,31 +106,53 @@ export class InputController {
     document.addEventListener(
       "keyup",
       (e) => {
-        e.preventDefault();
+        e.preventDefault(); // prevent default behavior to avoid unexpected page updates
         if (!e.repeat) {
           if (context.eventKeyMap[e.key] && context.eventKeyMap[e.key].hasOwnProperty("up")) {
-            context.eventKeyMap[e.key].up();
+            context.eventKeyMap[e.key].up(); // call up() for key event
+            remove(context.eventKeyStack, context.eventKeyMap[e.key]); // remove key event from the current stack
+            context.eventKeyStack.forEach((key) => key.down()); // call down in insertion order for each key event in the stack
           }
           if (context.eventCodeMap[e.code] && context.eventCodeMap[e.code].hasOwnProperty("up")) {
-            context.eventCodeMap[e.code].up();
+            context.eventCodeMap[e.code].up(); // call up() for code event
+            remove(context.eventCodeStack, context.eventCodeMap[e.code]); // remove code event from the current stack
+            context.eventCodeStack.forEach((code) => code.down()); // call down in insertion order for each code event in the stack
           }
         }
       },
       false
     );
+  }
+
+  /**
+   * Attaches click event handlers to mouse and touch event listeners.
+   */
+  attachClickListenersAndHandlers() {
+    let context = this;
+    Object.getOwnPropertyNames(this.clickMap).forEach((id) => {
+      let value = context.clickMap[id];
+      value.id = id; // set reference to id name
+      value.elem = document.getElementById(id); // get document element object
+      value.pressed = false; // set pressed state flag
+    });
+    const opts = { passive: false };
     for (const [_, el] of Object.entries(this.clickMap)) {
-      el.ref.addEventListener("mousedown", el.press, {
-        passive: false,
-      });
-      el.ref.addEventListener("touchstart", el.press, {
-        passive: false,
-      });
-      el.ref.addEventListener("mouseup", el.depress, {
-        passive: false,
-      });
-      el.ref.addEventListener("touchend", el.depress, {
-        passive: false,
-      });
+      el.elem.addEventListener("mousedown", el.press, opts);
+      el.elem.addEventListener("touchstart", el.press, opts);
+      el.elem.addEventListener("mouseup", el.depress, opts);
+      el.elem.addEventListener("touchend", el.depress, opts);
     }
+  }
+}
+
+/**
+ * Remove `element` from `array`.
+ * @param {Array} array Array of values.
+ * @param {any} element Element to remove from array.
+ */
+function remove(array, element) {
+  const index = array.indexOf(element);
+  if (index > -1) {
+    array.splice(index, 1);
   }
 }
