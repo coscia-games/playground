5fb648edcf240fb6bdbca6823d7ec710d7bb4dc5
AdamCoscia <47363758+AdamCoscia@users.noreply.github.com>
2021-05-15 19:51:12 -0400

    finish what I started

--- games/topdown/main.js
+++ games/topdown/main.js
@@ -35,8 +35,8 @@ import { GameState, GameEngine } from "../../modules/state.js";
         return this._elem.height / Resolution[this.ar].heights.slice(-1)[0]; // tile height
       },
       resize: function (isFullscreen = false) {
-        const targetWidth = isFullscreen ? window.innerWidth : this.$container.width();
-        const r = Resolution.getNearest(this.screen.ar, targetWidth, window.innerHeight);
+        const targetWidth = isFullscreen ? window.innerWidth : appContainer.$container.width();
+        const r = Resolution.getNearest(this.ar, targetWidth, window.innerHeight);
         pixiApp.renderer.resize(r.width, r.height); // resize renderer
         $("#res").html(`${r.width} x ${r.height}`); // update debug output
         if (Application.state == "running") setScene(); // re-draw scene
@@ -45,32 +45,38 @@ import { GameState, GameEngine } from "../../modules/state.js";
   };
 
   /**
+   * create assets map
+   */
+  var assets = {
+    sheet: {},
+    textures: {},
+  };
+
+  /**
    * create entities map
    */
   var entities = {
-    pc: {
+    player: {
       /** PIXI class instance members */
       _sprite: new PIXI.Sprite(), // PIXI.Sprite instance
       _debug: new PIXI.Text("unknown", { fontSize: 12 }), // PIXI.Text instance
       /** State */
       get direction() {
-        if (this.vx < 0 && this.vy < 0) this.direction = "up-left";
-        if (this.vx == 0 && this.vy < 0) this.direction = "up";
-        if (this.vx > 0 && this.vy < 0) this.direction = "up-right";
-        if (this.vx > 0 && this.vy == 0) this.direction = "right";
-        if (this.vx > 0 && this.vy > 0) this.direction = "down-right";
-        if (this.vx == 0 && this.vy > 0) this.direction = "down";
-        if (this.vx < 0 && this.vy > 0) this.direction = "down-left";
-        if (this.vx < 0 && this.vy == 0) this.direction = "left";
+        if (this.vx < 0 && this.vy < 0) return "up-left";
+        if (this.vx == 0 && this.vy < 0) return "up";
+        if (this.vx > 0 && this.vy < 0) return "up-right";
+        if (this.vx > 0 && this.vy == 0) return "right";
+        if (this.vx > 0 && this.vy > 0) return "down-right";
+        if (this.vx == 0 && this.vy > 0) return "down";
+        if (this.vx < 0 && this.vy > 0) return "down-left";
+        if (this.vx < 0 && this.vy == 0) return "left";
+        return "up"; // default direction
       },
       get visible() {
         return this._sprite.visible;
       },
-      hide: function () {
-        this._sprite.visible = false;
-      },
-      show: function () {
-        this._sprite.visible = true;
+      set visible(flag) {
+        this._sprite.visible = flag;
       },
       /** Position */
       get x() {
@@ -101,7 +107,7 @@ import { GameState, GameEngine } from "../../modules/state.js";
         this._sprite.vy = vy;
       },
     },
-    bg: {
+    background: {
       _container: new PIXI.Container(),
       _sprite: new PIXI.Sprite(),
     },
@@ -109,26 +115,6 @@ import { GameState, GameEngine } from "../../modules/state.js";
 
   // create new keyboard controller
   var kbc = new KeyboardController();
-  /** Up */
-  kbc.eventCodeMap.KeyW.down = () => pc.setVel(pc.vx, -1);
-  kbc.eventCodeMap.ArrowUp.down = () => pc.setVel(pc.vx, -1);
-  kbc.eventCodeMap.KeyW.up = () => pc.setVel(pc.vx, 0);
-  kbc.eventCodeMap.ArrowUp.up = () => pc.setVel(pc.vx, 0);
-  /** Down */
-  kbc.eventCodeMap.KeyS.down = () => pc.setVel(pc.vx, 1);
-  kbc.eventCodeMap.ArrowDown.down = () => pc.setVel(pc.vx, 1);
-  kbc.eventCodeMap.KeyS.up = () => pc.setVel(pc.vx, 0);
-  kbc.eventCodeMap.ArrowDown.up = () => pc.setVel(pc.vx, 0);
-  /** Left */
-  kbc.eventCodeMap.KeyA.down = () => pc.setVel(-1, pc.vy);
-  kbc.eventCodeMap.ArrowLeft.down = () => pc.setVel(-1, pc.vy);
-  kbc.eventCodeMap.KeyA.up = () => pc.setVel(0, pc.vy);
-  kbc.eventCodeMap.ArrowLeft.up = () => pc.setVel(0, pc.vy);
-  /** Right */
-  kbc.eventCodeMap.KeyD.down = () => pc.setVel(1, pc.vy);
-  kbc.eventCodeMap.ArrowRight.down = () => pc.setVel(1, pc.vy);
-  kbc.eventCodeMap.KeyD.up = () => pc.setVel(0, pc.vy);
-  kbc.eventCodeMap.ArrowRight.up = () => pc.setVel(0, pc.vy);
 
   /** PRIVATE METHODS */
 
@@ -139,9 +125,9 @@ import { GameState, GameEngine } from "../../modules/state.js";
    */
   function loadComplete(loader, resources) {
     // for (const [name, _] of Object.entries(resources)) {
-    //   assetsMap.textures[name] = resources[name].texture; // save texture
+    //   assets.textures[name] = resources[name].texture; // save texture
     // }
-    let sheet = resources["images/spritesheet.json"].spritesheet;
+    assets.sheet = resources["assets/spritesheet.json"].spritesheet;
     appContainer.screen.resize(); // resize pixi app screen
     appContainer.$container.empty().append(pixiApp.view); // add pixi app to the DOM
     Application.startGame(); // start the game!
@@ -149,13 +135,15 @@ import { GameState, GameEngine } from "../../modules/state.js";
 
   function setStage() {
     /** BACKGROUND */
-    bg._sprite = new PIXI.TilingSprite(assetsMap.textures["grass"]); // load bg sprite
+    let bg = entities.background; // alias
+    bg._sprite = new PIXI.TilingSprite(assets.sheet.textures["grass.png"]); // load background sprite
     bg._sprite.rotation = Math.PI / 4; // rotate background sprite
     bg._container.addChild(bg._sprite); // add tiling sprite to container
     bg._container.scale.y = Math.atan(Math.sin(30 * (Math.PI / 180))); // 0.463647609001 => 2:1 pixel ratio => dimetric
     pixiApp.stage.addChild(bg._container); // add container to stage
     /** PC */
-    pc._sprite = new PIXI.Sprite(assetsMap.textures["man"]); // load pc sprite
+    let pc = entities.player; // alias
+    pc._sprite = new PIXI.Sprite(assets.sheet.textures["man.png"]); // load pc sprite
     pc._sprite.anchor.set(0.5, 1); // in center-bottom of body
     pixiApp.stage.addChild(pc._sprite); // add pc to the stage
     pixiApp.stage.addChild(pc._debug); // add position text to the stage
@@ -163,17 +151,20 @@ import { GameState, GameEngine } from "../../modules/state.js";
 
   function setScene() {
     /** BACKGROUND */
-    bg._container.position.set(appContainer.screen.w / 3, 0);
+    let bg = entities.background; // alias
+    bg._container.position.set(appContainer.screen.w / 2, appContainer.screen.h / 2);
     bg._sprite.width = appContainer.screen.w;
     bg._sprite.height = appContainer.screen.h;
     bg._sprite.tileScale.set(appContainer.screen.tw, appContainer.screen.th); // scale bg sprites to tile width/height
     /** PC */
+    let pc = entities.player; // alias
     pc._sprite.scale.set(appContainer.screen.tw, appContainer.screen.th); // scale pc to tile width/height
     pc.setPos(appContainer.screen.w / 2, appContainer.screen.h / 2); // put pc in the center of the screen
     pc.setVel(0, 0); // start pc not moving
   }
 
   function drawScene(delta) {
+    let pc = entities.player; // alias
     pc.setPos(pc.x + pc.vx, pc.y + pc.vy); // update pc position using velocity
   }
 
@@ -184,6 +175,25 @@ import { GameState, GameEngine } from "../../modules/state.js";
   Application.init = function () {
     /** FLAGS */
     Application.state = "initializing"; // set app state flag
+
+    /** CONTROLS */
+    let pc = entities.player; // alias
+    kbc.eventCodeMap.KeyW.down = () => pc.setVel(pc.vx, -1);
+    kbc.eventCodeMap.ArrowUp.down = () => pc.setVel(pc.vx, -1);
+    kbc.eventCodeMap.KeyW.up = () => pc.setVel(pc.vx, 0);
+    kbc.eventCodeMap.ArrowUp.up = () => pc.setVel(pc.vx, 0);
+    kbc.eventCodeMap.KeyS.down = () => pc.setVel(pc.vx, 1);
+    kbc.eventCodeMap.ArrowDown.down = () => pc.setVel(pc.vx, 1);
+    kbc.eventCodeMap.KeyS.up = () => pc.setVel(pc.vx, 0);
+    kbc.eventCodeMap.ArrowDown.up = () => pc.setVel(pc.vx, 0);
+    kbc.eventCodeMap.KeyA.down = () => pc.setVel(-1, pc.vy);
+    kbc.eventCodeMap.ArrowLeft.down = () => pc.setVel(-1, pc.vy);
+    kbc.eventCodeMap.KeyA.up = () => pc.setVel(0, pc.vy);
+    kbc.eventCodeMap.ArrowLeft.up = () => pc.setVel(0, pc.vy);
+    kbc.eventCodeMap.KeyD.down = () => pc.setVel(1, pc.vy);
+    kbc.eventCodeMap.ArrowRight.down = () => pc.setVel(1, pc.vy);
+    kbc.eventCodeMap.KeyD.up = () => pc.setVel(0, pc.vy);
+    kbc.eventCodeMap.ArrowRight.up = () => pc.setVel(0, pc.vy);
 
     /** LISTENERS & HANDLERS */
     $(window).on("resize", () => {
@@ -196,7 +206,7 @@ import { GameState, GameEngine } from "../../modules/state.js";
     });
     $("#db-btn").on("click", () => {
       // toggle visibility of debug text
-      pc._debug.visible = !pc._debug.visible;
+      entities.player._debug.visible = !entities.player._debug.visible;
     });
     $("#ar-sel").on("change", function () {
       const ar = this.value; // get selected aspect ratio
@@ -210,7 +220,7 @@ import { GameState, GameEngine } from "../../modules/state.js";
     kbc.attachListenersAndHandlers(); // attach keyboard controller listeners and handlers
 
     /** METHODS */
-    // PIXI.Loader.shared.add(assetsMap.resources); // use resource map to specify sprites to load
+    // PIXI.Loader.shared.add(assets.resources); // use resource map to specify sprites to load
     PIXI.Loader.shared.add("assets/spritesheet.json"); // load spritesheet
     PIXI.Loader.shared.load(loadComplete); // load sprites, calling loadComplete() once finished
   };
